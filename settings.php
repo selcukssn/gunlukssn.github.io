de807294988fe63412d00ef1cb1c596fda8fd795
selcukssn <62655329+selcukssn@users.noreply.github.com>
2025-03-13 07:05:42 +0300

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+// Şifre hash'i (Aşağıdaki komutu kullanarak oluşturun:)
+// php -r 'echo password_hash("sizin_parolaniz", PASSWORD_DEFAULT);'
+define('PASSWORD_HASH', 'your_hashed_password_here');
+
+define('ENTRIES_FILE', 'entries.json');
+?>
