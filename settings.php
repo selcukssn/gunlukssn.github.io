cb410251ab4b46e7208f002b4d0ec3fa2057304c
selcukssn <62655329+selcukssn@users.noreply.github.com>
2025-03-13 07:12:22 +0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,3 @@
 <?php
-// Şifre hash'i (Aşağıdaki komutu kullanarak oluşturun:)
-// php -r 'echo password_hash("sizin_parolaniz", PASSWORD_DEFAULT);'
-define('PASSWORD_HASH', 'your_hashed_password_here');
-
 define('ENTRIES_FILE', 'entries.json');
 ?>
